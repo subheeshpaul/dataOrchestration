de9273ffac50697d891ac20a81865b8c5b78bea0
subheeshpaul <subheesh.paul@gmail.com>
2024-02-06 16:52:13 +0000

    promoted code

---
+++ transformers/green_taxi_transformer.py
@@ -0,0 +1,45 @@
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    """
+    Template code for a transformer block.
+
+    Add more parameters to this function if this block has multiple parent blocks.
+    There should be one parameter for each output variable from each parent block.
+
+    Args:
+        data: The output from the upstream parent block
+        args: The output from any additional upstream blocks (if applicable)
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your transformation logic here
+
+    data.columns =   (data.columns
+                       .str.replace('ID', '_id')
+                       .str.lower()
+                    )
+
+    dt = data[data['passenger_count'] > 0] 
+    dataFrame = dt[dt['trip_distance'] > 0]
+    return dataFrame
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output['passenger_count'].isin([0]).sum() == 0 , 'The are output with zero passengers'
+    assert output['trip_distance'].isin([0]).sum() == 0 , 'The are output with zero distance'
+   
+@test
+def vendor_id_column_exists(output, *args) -> None:
+    assert 'vendor_id' in output.columns, 'Column vendor_id does not exist.'
+
